ff125c253bd4bd143a530fe31fe2ba76dfb7a077
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-09-14 10:39:07 -0400

    Enabled Discovery Client

--- src/main/java/com/appsdeveloperblog/ws/api/photos/PhotosApplication.java
+++ src/main/java/com/appsdeveloperblog/ws/api/photos/PhotosApplication.java
@@ -2,12 +2,14 @@ package com.appsdeveloperblog.ws.api.photos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PhotosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotosApplication.class, args);
+    }
 
 }
